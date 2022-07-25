0b323f19afeb6d3934f68d06ad28f9d35a1d84f0
shobhji30 <imshobit33@gmail.com>
2022-07-26 00:40:55 +0530

    Updates

---
+++ precache-manifest.9bbfe95fd77656f3fad796775ed3d5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cf1622dfacc444f3a79",
+    "url": "/My-app/static/css/main.67aacbdc.chunk.css"
+  },
+  {
+    "revision": "0cf1622dfacc444f3a79",
+    "url": "/My-app/static/js/main.0cf1622d.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/My-app/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "62fe24661d9d6157a512",
+    "url": "/My-app/static/js/2.62fe2466.chunk.js"
+  },
+  {
+    "revision": "50d1c2787a7ca19f43bf",
+    "url": "/My-app/static/js/runtime~main.50d1c278.js"
+  },
+  {
+    "revision": "48d52326f0720c530895d6d95563e4b8",
+    "url": "/My-app/index.html"
+  }
+];
